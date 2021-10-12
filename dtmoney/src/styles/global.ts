126b4bca3f3f4a0c1e604fd5f928b667881521dd
Igor Klein <thelastklein@gmail.com>
2021-10-12 16:04:00 -0300

    Create global style

---
+++ dtmoney/src/styles/global.ts
@@ -0,0 +1,48 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    
+    :root{
+        --background: #f8f2f5;
+        --red: #E52E4D;
+        --blue: #5429CC;
+
+        --blue-light: #6933FF;
+
+        --text-title: #363F5F;
+        --text-body:  #969CB3;
+
+        --background: #F8F2F5;
+        --shape: #FFFFFF;
+    }
+
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: Border-box;
+    }
+
+    html{
+        @media (max-width: 1080px){
+            font-size: 93.75%;
+        }
+
+        @media (max-width: 720px){
+            font-size: 87.5%;
+        }
+    }
+
+    body{
+        background: var(--background);
+        -webkit-font-smoothing: antialised;
+    }
+
+    button{
+        cursor: pointer;
+    }
+
+    [disabled]{
+        opacity: 0.6;
+        cursor: not-allowed;
+    }
+`
