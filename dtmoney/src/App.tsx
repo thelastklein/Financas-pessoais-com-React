973e49de96ece2ccfb0427a405c12a87b1797eee
Igor Klein <thelastklein@gmail.com>
2022-01-22 16:10:31 -0300

    Create components summary and dashboard

--- dtmoney/src/App.tsx
+++ dtmoney/src/App.tsx
@@ -1,10 +1,12 @@
 import { GlobalStyle } from "./styles/global";
 import { Header } from "./components/header";
+import { Dashboard } from "./components/dashboard";
 
 export function App() {
   return (
     <>
      <Header/>
+     <Dashboard/>
      <GlobalStyle/>
     </>
   );
---
+++ dtmoney/src/components/summary/index.tsx
@@ -0,0 +1,36 @@
+import arrowupimg from '../../assets/arrow-up.svg'
+import arrowdownimg from '../../assets/arrow-down.svg'
+import moneyimg from '../../assets/money.svg'
+
+import { Container } from "./styles";
+
+
+export function Summary(){
+    return(
+        <Container>
+            <div>
+                <header>
+                    <p>Entradas</p>
+                    <img src={arrowupimg} alt="Entradas"/>
+                </header>
+                <strong>R$1000,00</strong>
+            </div>
+
+            <div>
+                <header>
+                    <p>Saídas</p>
+                    <img src={arrowdownimg} alt="Saidas"/>
+                </header>
+                <strong>-R$500,00</strong>
+            </div>
+
+            <div className="highlight-background">
+                <header>
+                    <p>Total</p>
+                    <img src={moneyimg} alt="Total"/>
+                </header>
+                <strong>R$500,00</strong>
+            </div>
+        </Container>
+    )
+}
